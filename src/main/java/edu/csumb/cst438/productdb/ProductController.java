72a347bf4f38f0ed4fa046959c5e27ceb32e9289
voidstar23 <malasadi@csumb.edu>
2018-11-24 13:27:09 -0800

    CORS

--- src/main/java/edu/csumb/cst438/productdb/ProductController.java
+++ src/main/java/edu/csumb/cst438/productdb/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import edu.csumb.cst438.productdb.entities.Product;
 
 @RestController
@@ -14,13 +14,14 @@ public class ProductController {
     @Autowired
     IProductRepository ProductRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping ("/GetAllProducts")
     public List<Product> getAll () {
         List<Product> result = ProductRepository.findAll();
         return result;
     }
 
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/id/{id}")
     public Product getProductById (@PathVariable String id) {
         Product result = ProductRepository.findByRepoId(id);
